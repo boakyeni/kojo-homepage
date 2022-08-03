1b1f39b1f5308566aec90ae2908fceb8aa2c75a3
Kojo Nimako <boakyeni@usc.edu>
2022-08-02 20:43:52 -0700

    embeded pdf to chaos page

--- pages/works/chaos.js
+++ pages/works/chaos.js
@@ -1,8 +1,9 @@
-import {Container, Badge, Link, List, ListItem} from '@chakra-ui/react'
+import { Container, Badge, Link, List, ListItem, AspectRatio } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
-import {Title, WorkImage, Meta } from '../../components/work'
+import { Title, WorkImage, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
+
 
 const Work = () => {
     return (
@@ -16,14 +17,16 @@ const Work = () => {
                 </P>
                 <List ml={4} my={4}>
                     <ListItem>
-                        <Meta>Website</Meta>
-                    <Link href='/'>
-                        home <ExternalLinkIcon mx="2px" />
-                    </Link>
+                        <Meta>Google Drive</Meta>
+                        <Link href='https://drive.google.com/file/d/1YGmY6PcDLUQw49Z64UDM6HE-5ZJ6jl8I/view?usp=sharing' isExternal>
+                            View PDF <ExternalLinkIcon mx="2px" />
+                        </Link>
                     </ListItem>
                     <ListItem></ListItem>
-                    <ListItem></ListItem>
                 </List>
+                <AspectRatio maxW='560px' ratio={1}>
+                    <iframe src="https://drive.google.com/file/d/1YGmY6PcDLUQw49Z64UDM6HE-5ZJ6jl8I/preview" allow="autoplay"></iframe>
+                </AspectRatio>
             </Container>
         </Layout>
     )
